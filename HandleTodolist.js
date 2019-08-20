6810b9080cf6a70dc681569917456c78c3b2b4a4
4charles2 <ctognol@laposte.net>
2019-08-20 15:42:43 +0200

    init app todolist

---
+++ HandleTodolist.js
@@ -0,0 +1,77 @@
+/**
+ * Gestionnaire de la todolist
+ * Effectue les ajout supression et / ou modification de la liste des tâches
+ * @author Tognol Charles
+ * @description class de gestion des evenements de la todolist
+ * @summary class gestion de todolist
+ * @version 0.1.0
+ * @tutorial me
+ *
+ * @file class HandleTodoList
+ * @licence MIT
+ *
+ *
+ * @since 0.1.0
+ * @copyright Tognol Charles ©2019
+ * @see [charles-tognol]{@link https://charles-tognol.fr}
+ */
+class HandleTodolist {
+
+    constructor() {
+        this.todolist = []
+    }
+
+    /**
+     * Ajoute la tâche à la todolist si test OK
+     * Puis execute une des fonctions callbacks
+     * @param {string} task
+     * @param {{'error': function, 'work': function}} callbacks de callbacks @typedef {}
+     *
+     * @exemple
+     *      add('manger', {'error': function(), 'work': function()})
+     * @see [charles-tognol]{@link https://charles-tognol.fr}
+     */
+    add(task, callbacks) {
+        if (HandleTodolist.isEmpty(task))
+            callbacks.error("notWork", "la tâche est vide");
+        else if (this.isInclude(task))
+            callbacks.error("notWork", "la tâche existe déjà");
+        else {
+            this.todolist.push(task);
+            callbacks.work("todoList", this.todolist)
+        }
+    }
+
+    /**
+     * Suprime une tâche de la todolist après avoir effectue les verifications
+     * Puis execute les fonctions callbacks
+     *
+     * @param task
+     * @param {{'error': function, 'work': function}} callbacks de callbacks
+     *
+     *  @exemple
+     *      delete('manger', {'error': function, 'work': function})
+     *  @author Tognol Charles
+     */
+    delete(task, callbacks) {
+        if(!HandleTodolist.isEmpty(task) && this.isInclude(task)) {
+            this.todolist.splice(this.todolist.indexOf(task));
+            callbacks.work("todolist", this.todolist)
+        }else{
+            callbacks.error('notwork', "La tâche est déjà suprimé")
+        }
+    }
+
+    isInclude(task) {
+        return this.todolist.includes(task)
+    }
+
+    static isEmpty(task) {
+        return task === '' || task === undefined || task === null || task === 'undefined'
+    }
+    get show() {
+        return this.todolist
+    }
+}
+
+module.exports = HandleTodolist;
---
+++ app.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+
+
+let ent = require('ent');
+let HandleTodolist = require('./HandleTodolist');
+
+var todoList = new HandleTodolist();
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+
+    res.sendFile(__dirname+'/index.html')
+});
+
+io.on('connection', function(socket){
+    //callbacks pour les tâches class Todolist
+    let callbacks = {'error': (name, value) => {socket.emit(name, value)}, 'work': (name, value) => {io.emit(name, value)}};
+
+    //Si un utilisateur veux supprimer une tâche
+    socket.on('deleteTask', function (message) {
+        todoList.delete(message,callbacks)
+    });
+    //Si l'utilisateur veut créer une nouvelle tâche
+    socket.on('addTask', function (message) {
+        let task = ent.encode(message);
+        //Fait les test si le message est valide puis envoie un message d'erreur
+        //à l'user si erreur
+        //Sinon si les tests sont passé avec succes alors renvoie la todolist à tous les utilisateurs
+        todoList.add(task,callbacks)
+    });
+    //Quand l'utilisateur se connecte on lui envoye la todolist
+    socket.emit('connexion', todoList.show)
+});
+
+app.use((req,res)=>{
+    res.redirect('/')
+});
+server.listen(8080);
